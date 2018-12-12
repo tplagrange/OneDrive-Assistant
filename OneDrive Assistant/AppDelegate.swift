ca1a429d1990ebfee93dd2f71b44be8bd34699a3
Thomas Lagrange <tplagrange@gmail.com>
2018-12-12 09:12:25 -0500

    Initial Commit

---
+++ OneDrive Assistant/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OneDrive Assistant
+//
+//  Created by Lagrange, Thomas on 12/12/18.
+//  Copyright © 2018 Thomas Lagrange. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OneDrive Assistant/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OneDrive Assistant
+//
+//  Created by Lagrange, Thomas on 12/12/18.
+//  Copyright © 2018 Thomas Lagrange. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
